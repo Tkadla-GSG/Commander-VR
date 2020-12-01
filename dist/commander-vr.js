277181508f7f5783ad48b8511b897429c3e27c7e
Jakub Tkadleček <jakub@mewssystems.com>
2020-12-02 00:27:51 +0100

    start from scratch

---
+++ dist/commander-vr.js
@@ -0,0 +1,11 @@
+/******/ (() => { // webpackBootstrap
+/*!**************************!*\
+  !*** ./scripts/index.ts ***!
+  \**************************/
+/*! unknown exports (runtime-defined) */
+/*! runtime requirements:  */
+document.getElementById('element').setAttribute('style', 'color: blue');
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9Db21tYW5kZXJWUi8uL3NjcmlwdHMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7O0FBQUEiLCJmaWxlIjoiY29tbWFuZGVyLXZyLmpzIiwic291cmNlc0NvbnRlbnQiOlsiZG9jdW1lbnQuZ2V0RWxlbWVudEJ5SWQoJ2VsZW1lbnQnKS5zZXRBdHRyaWJ1dGUoJ3N0eWxlJywgJ2NvbG9yOiBibHVlJyk7XHJcbiJdLCJzb3VyY2VSb290IjoiIn0=
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+var path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: "./scripts/index.ts",
+    devtool: 'inline-source-map',
+    output: {
+        filename: "commander-vr.js"
+    },
+    resolve: {
+        extensions: [ '.ts', '.js' ],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+}
