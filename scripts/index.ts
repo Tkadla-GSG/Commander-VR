fc86ff87356ace0492f6095041985bb43580d3b6
Jakub Tkadleček <jakub@mewssystems.com>
2020-12-05 18:42:53 +0100

    add basic gui

--- scripts/index.ts
+++ scripts/index.ts
@@ -1,5 +1,5 @@
 import { Engine, EnvironmentHelper, Scene, HemisphericLight, Vector3, Mesh, FreeCamera } from '@babylonjs/core';
-import { InputPassword, InputText, VirtualKeyboard, StackPanel, Control, AdvancedDynamicTexture } from '@babylonjs/gui';
+import { InputPassword, InputText, VirtualKeyboard, StackPanel, Button, AdvancedDynamicTexture } from '@babylonjs/gui';
 
 class VRApp {
   private _engine: Engine;
@@ -24,8 +24,10 @@ class VRApp {
     camera.attachControl(canvas, true);
 
     // GUI
-    /*
-    const advancedTexture = AdvancedDynamicTexture.CreateFullscreenUI("UI");
+    const uiPlane = Mesh.CreatePlane("UI", 4, this._scene);
+    uiPlane.position = new Vector3(2, 1.6, 0);
+    uiPlane.rotation = new Vector3(0, Math.PI/2, 0);
+    const advancedTexture = AdvancedDynamicTexture.CreateForMesh(uiPlane);
     const panel = new StackPanel();
     
     const userNameField = new InputText();
@@ -35,6 +37,7 @@ class VRApp {
     userNameField.text = "sample-admin";
     userNameField.color = "white";
     userNameField.background = "green";
+    // userNameField.paddingBottom = "10px";
 
     panel.addControl(userNameField);
 
@@ -45,16 +48,34 @@ class VRApp {
     passwordFiled.text = "sample";
     passwordFiled.color = "white";
     passwordFiled.background = "green";
+    // userNameField.paddingBottom = "10px";
 
     panel.addControl(passwordFiled);
 
+    const signInButton = Button.CreateSimpleButton("button", "Sign in");
+    signInButton.onPointerClickObservable.add(() =>  {
+        // sign in
+    });
+    signInButton.top = "0px";
+    signInButton.left = "0px";
+    signInButton.width = "150px";
+    signInButton.height = "50px";
+    signInButton.cornerRadius = 20;
+    signInButton.thickness = 4;
+    signInButton.children[0].color = "#DFF9FB";
+    signInButton.children[0].fontSize = 24;
+    signInButton.color = "#FF7979";
+    signInButton.background = "#EB4D4B";
+    panel.addControl(signInButton);
+
     var keyboard = VirtualKeyboard.CreateDefaultLayout();
     keyboard.connect(userNameField);
+    keyboard.connect(passwordFiled);
 
     panel.addControl(keyboard);
 
     advancedTexture.addControl(panel);
-*/
+
     for (let i = 0; i < 25; i++) {
       const box = Mesh.CreateBox(`box_${i}`, 2, this._scene);
       box.position = new Vector3(Math.random() * 50.0 - 25.0, 1, Math.random() * 50.0 - 25.0);
